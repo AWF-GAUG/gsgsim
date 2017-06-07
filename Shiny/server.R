bd67602ba59ca6fc6cdff78dc4781a3c155d3a9d
L. Fehrmann <L.Fehrmann@UFFE34.uni-forst.gwdg.de>
2017-06-07 08:09:04 +0200

    Shiny app was moved to gsgsim

---
+++ Shiny/server.R
@@ -0,0 +1,67 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+
+library(shiny)
+library(gsgsim)
+
+# Define server logic required to generate GSG
+shinyServer(function(input, output) {
+
+
+  output$gsgPlot <- renderPlot({
+
+
+    # Take a dependency on input$goButton
+    input$go
+
+    withProgress(message = 'Work in progress', value = 0,{
+
+      # load selected aoi as boundary
+      in_bnd <- input$aoi
+      if (is.null(in_bnd)) {
+        bnd_path <- NULL
+      } else {
+        bnd_path <- in_bnd$datapath
+      }
+
+      # Increment the progress bar, and update the detail text.
+      incProgress(1/3, detail = paste("Dowloading boundary (can take a while...)"), 1)
+
+      bnd <- isolate(load_boundary(x = bnd_path,
+                                   country_code = input$country_code,
+                                   adm_level = 0));
+
+      # generate GSG based on inputs from ui.R
+      # isolate () to avoid dependency on input$dist (but reactive on button)
+      gsg <-isolate(gen_gsg(input$dist, bnd));
+
+      # Increment the progress bar, and update the detail text.
+      incProgress(2/3, detail = paste("Generating GSG"), 2)
+
+
+      # draw a map with generated GSG
+      plot(bnd)
+      plot(gsg, pch=20, col="red", add=TRUE)
+
+      # Increment the progress bar, and update the detail text.
+      incProgress(3/3, detail = paste("Plotting"), 3)
+
+      # Download
+      # Take a dependency on input$downloadButton
+      # input$download
+
+      #
+
+
+    })
+
+  })
+
+})
---
+++ Shiny/ui.R
@@ -0,0 +1,70 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(raster)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Generate GSG"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      # Inputs
+
+      # Example input country code
+      selectInput("country_code", "Select country",
+                  c("World", raster::ccodes()[, 1]),
+                  c("world", raster::ccodes()[, 2])),
+
+      # Input administrative level
+      selectInput("adm_level", "administrative level",
+                  c("Level 0" = "0",
+                    "Level 1" = "1",
+                    "Level 2" = "2")),
+
+      # Example file input for aoi
+      fileInput("aoi",
+                "Upload specific aoi (.shp or .kml)",
+                multiple = FALSE,
+                accept = c(".shp", ".kml")),
+      hr(),
+
+      # grid distance in km
+      numericInput("dist", "Grid distance", 250),
+
+      # Button "generate"
+      actionButton("go", "generate"),
+
+      hr(),
+
+      # selection of output format
+      selectInput("format", "File format",
+                  c("KML" = ".kml",
+                    "ESRI Shapefile" = ".shp",
+                    "CSV" = ".csv")),
+
+      # Button "generate"
+      actionButton("download", "download")
+
+     ),
+
+
+
+    # Show a plot of the generated grid
+    mainPanel(
+       plotOutput("gsgPlot", width = "100%")
+
+
+    )
+  )
+))
