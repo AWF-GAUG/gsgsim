10055eeb358caeeca5e6b7de31e865fd38b18857
L. Fehrmann <L.Fehrmann@UFFE34.uni-forst.gwdg.de>
2017-06-20 15:41:55 +0200

    code cleaned

--- Shiny/server.R
+++ Shiny/server.R
@@ -17,21 +17,22 @@ library(DT)
 
 
 options(shiny.error = browser)
+
 # Define server logic required to generate GSG
+
 shinyServer(function(input, output, session) {
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
-      setView(lng = 0,
-              lat = 20,
-              zoom = 3) %>%
-      addWMSTiles(
-        "http://globalforestwatch-624153201.us-west-1.elb.amazonaws.com:80/arcgis/services/TreeCover2000/ImageServer/WMSServer",
+      setView(lng = 0, lat = 20, zoom = 3) %>%
+      addWMSTiles("http://globalforestwatch-624153201.us-west-1.elb.amazonaws.com:80/arcgis/services/TreeCover2000/ImageServer/WMSServer",
         layers = "0",
-        options = WMSTileOptions(format = "image/png", transparent = TRUE)
-      ) #%>% addWMSLegend("http://globalforestwatch-624153201.us-west-1.elb.amazonaws.com:80/arcgis/services/TreeCover2000/ImageServer/WMSServer?request=GetLegendGraphic%26version=1.3.0%26format=image/png%26layer=0",
-    #position = 'bottomright')
+        options = WMSTileOptions(format = "image/png", transparent = TRUE),
+        attribution = 'test'
+      ) #%>%
 
+      #addWMSLegend("http://globalforestwatch-624153201.us-west-1.elb.amazonaws.com:80/arcgis/services/TreeCover2000/ImageServer/WMSServer?request=GetLegendGraphic%26version=1.3.0%26format=image/png%26layer=0",
+      #position = 'bottomright')
 
   })
 
@@ -41,6 +42,7 @@ shinyServer(function(input, output, session) {
 
   observeEvent(# Take a dependency on input$goButton
     input$go, {
+
       progress <- Progress$new(session, min = 1, max = 4)
       on.exit(progress$close())
 
@@ -57,10 +59,10 @@ shinyServer(function(input, output, session) {
                'kml' = {
                  dir <- dirname(in_bnd[1, 4])
                  file.rename(in_bnd[1, 4], paste0(dir, "/", in_bnd[1, 1]))
-                 getaoi <-
-                   list.files(dir, pattern = "*.kml", full.names = TRUE)
 
+                 getaoi <- list.files(dir, pattern = "*.kml", full.names = TRUE)
                },
+
                'shp' = {
                  dir <- dirname(in_bnd[1, 4])
 
@@ -68,8 +70,7 @@ shinyServer(function(input, output, session) {
                    file.rename(in_bnd[i, 4], paste0(dir, "/", in_bnd[i, 1]))
                  }
 
-                 getaoi <-
-                   list.files(dir, pattern = "*.shp", full.names = TRUE)
+                 getaoi <- list.files(dir, pattern = "*.shp", full.names = TRUE)
 
                })
       }
@@ -80,7 +81,6 @@ shinyServer(function(input, output, session) {
         adm_level = 0
       ))
 
-
       # Increment the progress bar, and update the detail text.
       progress$set(value = 2,
                    message = 'Calculation in progress',
@@ -88,11 +88,12 @@ shinyServer(function(input, output, session) {
 
       # generate GSG based on inputs from ui.R
       # isolate () to avoid dependency on input$dist (but reactive on button)
+
       gsg <- isolate(gen_gsg(input$dist, bnd))
 
-
+      # Show resulting sample size
       output$text1 = renderText({
-        paste0("Generated points: ", length(gsg))
+        paste0("Number of generated points: ", length(gsg))
       })
 
       if (length(gsg) == 1)
@@ -115,8 +116,11 @@ shinyServer(function(input, output, session) {
                      detail = 'Gerenating outputs...')
 
 
-        # draw a map with generated GSG
-        leafletProxy("map") %>%
+        ## Interactive Map ###########################################
+
+        # Create the map
+
+        leafletProxy("map", data = gsg) %>%
           clearShapes() %>%
 
           addPolygons(
@@ -137,12 +141,7 @@ shinyServer(function(input, output, session) {
             fillOpacity = 0.9
           ) %>%
 
-
           setView(center$x, center$y, zoom = 5)
-        #%>%
-        #setMaxBounds(lng1 = xmax(gsg),lat1 = ymax(gsg),
-        #          lng2 = ymin(gsg),lat2 = ymin(gsg))
-
 
         updateNavbarPage(session, "nav", selected = "gomap")
 
@@ -155,6 +154,7 @@ shinyServer(function(input, output, session) {
                      detail = 'Plotting the GSG...')
 
       }
+    })
 
 
       ### Export GSG ###
@@ -192,5 +192,4 @@ shinyServer(function(input, output, session) {
                        }
                      )
                    })
-    })
 })
