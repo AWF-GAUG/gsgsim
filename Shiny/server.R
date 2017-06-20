3b4044d24022632847a967d95ba48c88ead7550e
L. Fehrmann <L.Fehrmann@UFFE34.uni-forst.gwdg.de>
2017-06-20 14:30:58 +0200

    code formatted

--- Shiny/server.R
+++ Shiny/server.R
@@ -19,34 +19,33 @@ library(DT)
 options(shiny.error = browser)
 # Define server logic required to generate GSG
 shinyServer(function(input, output, session) {
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      setView(lng = 0,
+              lat = 20,
+              zoom = 3) %>%
+      addWMSTiles(
+        "http://globalforestwatch-624153201.us-west-1.elb.amazonaws.com:80/arcgis/services/TreeCover2000/ImageServer/WMSServer",
+        layers = "0",
+        options = WMSTileOptions(format = "image/png", transparent = TRUE)
+      ) #%>% addWMSLegend("http://globalforestwatch-624153201.us-west-1.elb.amazonaws.com:80/arcgis/services/TreeCover2000/ImageServer/WMSServer?request=GetLegendGraphic%26version=1.3.0%26format=image/png%26layer=0",
+    #position = 'bottomright')
 
 
-output$map <- renderLeaflet({
-  leaflet() %>%
-    addTiles() %>%
-    setView(lng = 0, lat = 20, zoom = 3)%>%
-    addWMSTiles(
-      "http://globalforestwatch-624153201.us-west-1.elb.amazonaws.com:80/arcgis/services/TreeCover2000/ImageServer/WMSServer",
-      layers="0",
-      options = WMSTileOptions(format = "image/png", transparent = TRUE)
-    ) #%>% addWMSLegend("http://globalforestwatch-624153201.us-west-1.elb.amazonaws.com:80/arcgis/services/TreeCover2000/ImageServer/WMSServer?request=GetLegendGraphic%26version=1.3.0%26format=image/png%26layer=0",
-      #position = 'bottomright')
+  })
 
+  observeEvent(input$reset_input, {
+    shinyjs::reset("settings")
+  })
 
-})
-
-observeEvent(input$reset_input,{
-  shinyjs::reset("settings")
-})
-
-observeEvent(
-    # Take a dependency on input$goButton
+  observeEvent(# Take a dependency on input$goButton
     input$go, {
-
-      progress <- Progress$new(session, min=1, max=4)
+      progress <- Progress$new(session, min = 1, max = 4)
       on.exit(progress$close())
 
-      progress$set(value = 1, message = 'Calculation in progress',
+      progress$set(value = 1,
+                   message = 'Calculation in progress',
                    detail = 'Downloading boundary...')
 
       # load selected aoi as boundary
@@ -56,115 +55,142 @@ observeEvent(
       } else {
         switch(input$inputformat,
                'kml' = {
-                 dir<-dirname(in_bnd[1,4])
-                 file.rename(in_bnd[1,4], paste0(dir,"/",in_bnd[1,1]))
-                 getaoi <- list.files(dir, pattern="*.kml", full.names=TRUE)
+                 dir <- dirname(in_bnd[1, 4])
+                 file.rename(in_bnd[1, 4], paste0(dir, "/", in_bnd[1, 1]))
+                 getaoi <-
+                   list.files(dir, pattern = "*.kml", full.names = TRUE)
 
                },
                'shp' = {
+                 dir <- dirname(in_bnd[1, 4])
 
-                dir<-dirname(in_bnd[1,4])
+                 for (i in 1:nrow(in_bnd)) {
+                   file.rename(in_bnd[i, 4], paste0(dir, "/", in_bnd[i, 1]))
+                 }
 
-                for ( i in 1:nrow(in_bnd)) {
-                   file.rename(in_bnd[i,4], paste0(dir,"/",in_bnd[i,1]))}
+                 getaoi <-
+                   list.files(dir, pattern = "*.shp", full.names = TRUE)
 
-                getaoi <- list.files(dir, pattern="*.shp", full.names=TRUE)
-
-            })
+               })
       }
 
-      bnd <- isolate(load_boundary(x = getaoi,
-                                   country_code = input$country_code,
-                                   adm_level = 0));
+      bnd <- isolate(load_boundary(
+        x = getaoi,
+        country_code = input$country_code,
+        adm_level = 0
+      ))
+
 
       # Increment the progress bar, and update the detail text.
-      progress$set(value = 2, message = 'Calculation in progress',
+      progress$set(value = 2,
+                   message = 'Calculation in progress',
                    detail = 'initializing GSG generation...')
 
       # generate GSG based on inputs from ui.R
       # isolate () to avoid dependency on input$dist (but reactive on button)
-      gsg <- isolate(gen_gsg(input$dist, bnd));
+      gsg <- isolate(gen_gsg(input$dist, bnd))
 
-      output$text1 = renderText({paste0("Generated points: ",length(gsg))})
 
-      if(length(gsg)==1)
+      output$text1 = renderText({
+        paste0("Generated points: ", length(gsg))
+      })
+
+      if (length(gsg) == 1)
       {
-        showModal(modalDialog(
-        title = "Warning",
-        "No sample locations in the area of interest! Adjust grid distance!",
-        easyClose = FALSE
-        ))
+        showModal(
+          modalDialog(
+            title = "Warning",
+            "No sample locations in the area of interest! Adjust grid distance!",
+            easyClose = FALSE
+          )
+        )
 
-      }else{
-
-      center=gCentroid(gsg)
-
-
-      # Increment the progress bar, and update the detail text.
-      progress$set(value = 3, message = 'Calculation in progress',
-                   detail = 'Gerenating outputs...')
+      } else{
+        center = gCentroid(gsg)
 
 
-      # draw a map with generated GSG
-      leafletProxy("map") %>%
-        clearShapes() %>%
-
-        addPolygons(data = bnd, color = "#444444", weight = 1, smoothFactor = 0.5,
-                    opacity = 0.3, fillOpacity = 0.5) %>%
-
-        addCircles(data=gsg, weight = 3, radius=40,
-                  color="#CD0000", stroke = TRUE, fillOpacity = 0.9) %>%
+        # Increment the progress bar, and update the detail text.
+        progress$set(value = 3,
+                     message = 'Calculation in progress',
+                     detail = 'Gerenating outputs...')
 
 
-        setView(center$x, center$y, zoom=5)
+        # draw a map with generated GSG
+        leafletProxy("map") %>%
+          clearShapes() %>%
+
+          addPolygons(
+            data = bnd,
+            color = "#444444",
+            weight = 1,
+            smoothFactor = 0.5,
+            opacity = 0.3,
+            fillOpacity = 0.5
+          ) %>%
+
+          addCircles(
+            data = gsg,
+            weight = 3,
+            radius = 40,
+            color = "#CD0000",
+            stroke = TRUE,
+            fillOpacity = 0.9
+          ) %>%
+
+
+          setView(center$x, center$y, zoom = 5)
         #%>%
         #setMaxBounds(lng1 = xmax(gsg),lat1 = ymax(gsg),
         #          lng2 = ymin(gsg),lat2 = ymin(gsg))
 
 
-      updateNavbarPage(session, "nav", selected="gomap")
+        updateNavbarPage(session, "nav", selected = "gomap")
 
-      # Generate Data Table
-      output$mytable <- DT::renderDataTable(
-        DT::datatable(as.data.frame(gsg), options = list(pageLength = 25))
-      )
+        # Generate Data Table
+        output$mytable <- DT::renderDataTable(DT::datatable(as.data.frame(gsg), options = list(pageLength = 25)))
 
-      # Increment the progress bar, and update the detail text.
-      progress$set(value = 4, message = 'Calculation in progress',
-                   detail = 'Plotting the GSG...')
+        # Increment the progress bar, and update the detail text.
+        progress$set(value = 4,
+                     message = 'Calculation in progress',
+                     detail = 'Plotting the GSG...')
 
       }
 
 
       ### Export GSG ###
 
-     observeEvent(input$format,
-      if(input$format == "shp"){
-
-      output$download <- downloadHandler(
-          filename = 'GSGExport.zip',
-          content = function(file) {
-           if (length(Sys.glob("GSGExport.*"))>0){
-             file.remove(Sys.glob("GSGExport.*"))
-            }
-            writeOGR(gsg, dsn="GSGExport.shp", layer="GSGExport", driver="ESRI Shapefile")
-            zip(zipfile='GSGExport.zip', files=Sys.glob("GSGExport.*"))
-            file.copy("GSGExport.zip", file)
-            if (length(Sys.glob("GSGExport.*"))>0){
-              file.remove(Sys.glob("GSGExport.*"))
-            }
-          })
-      }
-      else if (input$format == "kml"){
-
-        output$download <- downloadHandler(
-          filename = function() {
-          paste0("GSG", input$dist, ".kml")
-          },
-          content = function(file) {
-          writeOGR(gsg, file, layer = paste0("GSG",input$dist, "_KML"), driver = "KML")
-          })
-      }
-     )
+      observeEvent(input$format,
+                   if (input$format == "shp") {
+                     output$download <- downloadHandler(
+                       filename = 'GSGExport.zip',
+                       content = function(file) {
+                         if (length(Sys.glob("GSGExport.*")) > 0) {
+                           file.remove(Sys.glob("GSGExport.*"))
+                         }
+                         writeOGR(gsg,
+                                  dsn = "GSGExport.shp",
+                                  layer = "GSGExport",
+                                  driver = "ESRI Shapefile")
+                         zip(zipfile = 'GSGExport.zip', files = Sys.glob("GSGExport.*"))
+                         file.copy("GSGExport.zip", file)
+                         if (length(Sys.glob("GSGExport.*")) > 0) {
+                           file.remove(Sys.glob("GSGExport.*"))
+                         }
+                       }
+                     )
+                   }
+                   else if (input$format == "kml") {
+                     output$download <- downloadHandler(
+                       filename = function() {
+                         paste0("GSG", input$dist, ".kml")
+                       },
+                       content = function(file) {
+                         writeOGR(gsg,
+                                  file,
+                                  layer = paste0("GSG", input$dist, "_KML"),
+                                  driver = "KML")
+                       }
+                     )
+                   })
     })
-    })
+})
--- Shiny/ui.R
+++ Shiny/ui.R
@@ -15,55 +15,76 @@ library(DT)
 
 
 
+
 ui <- fluidPage(
   shinyjs::useShinyjs(),
 
-# Define UI for application that draws a histogram
-navbarPage("Global Sampling Grid", id="nav",
 
-  tabPanel("Interactive map", value="gomap",
+  navbarPage(
+    "Global Sampling Grid",
+    id = "nav",
 
-  div(class="outer",
-    tags$head(
-      # Include our custom CSS
-      includeCSS("styles.css")
-      ),
-      leafletOutput("map", height = "100%", width = "100%")
-    )
-  ),
+    tabPanel(
+      "Interactive map",
+      value = "gomap",
 
-  tabPanel("Data Explorer",
-         DT::dataTableOutput('mytable')
-  ),
+      div(
+        class = "outer",
+        tags$head(# Include our custom CSS
+          includeCSS("styles.css")),
 
-  tabPanel("Generate GSG",
-           div(id="settings",
-            fluidRow(
-              column(4,
+        leafletOutput("map", height = "100%", width = "100%")
+      )
+    ),
+
+    tabPanel("Data Explorer",
+             DT::dataTableOutput('mytable')),
+
+    tabPanel("Generate GSG",
+
+             div(id = "settings",
+                 fluidRow(
+                   column(4,
                      # Inputs
                      h4("GSG Settings"),
-                     # Example input country code
-                     selectInput("country_code", "Select countries",
-                                 c("World", raster::ccodes()[, 1]),
-                                 c("world", raster::ccodes()[, 2]),
-                                 multiple = TRUE),
+
+                     # Select country code
+                     selectInput(
+                       "country_code",
+                       "Select countries",
+                       c("World", raster::ccodes()[, 1]),
+                       c("world", raster::ccodes()[, 2]),
+                       multiple = TRUE
+                     ),
 
                      # Input administrative level
-                     selectInput("adm_level", "Administrative level",
-                                 c("Level 0" = "0",
-                                   "Level 1" = "1",
-                                   "Level 2" = "2")),
+                     selectInput(
+                       "adm_level",
+                       "Administrative level",
+                       c(
+                         "Level 0" = "0",
+                         "Level 1" = "1",
+                         "Level 2" = "2"
+                       )
+                     ),
 
-                     radioButtons(inputId = 'inputformat',
-                                  label = 'Upload specific aoi as shapefile (epsg:4326) or KML',
-                                  choices = c('Shapefile' = 'shp', 'KML' = 'kml'), inline = TRUE),
-                     # Example file input for aoi
-                     fileInput("aoi", "For .shp upload select the .shp, .prj, .shx and .dbf file simultaneously!",
-                               accept=c('.shp','.dbf','.sbn','.sbx','.shx',".prj",".kml"),
-                               multiple=TRUE)
-              ),
+                     radioButtons(
+                       inputId = 'inputformat',
+                       label = 'Upload specific aoi as shapefile (epsg:4326) or KML',
+                       choices = c('Shapefile' = 'shp', 'KML' = 'kml'),
+                       inline = TRUE
+                     ),
 
-              column(4,
+                     # File input for aoi
+                     fileInput(
+                       "aoi",
+                       "For .shp upload select the .shp, .prj, .shx and .dbf file simultaneously!",
+                       accept = c('.shp', '.dbf', '.sbn', '.sbx', '.shx', ".prj", ".kml"),
+                       multiple = TRUE
+                     )
+                   ),
+
+                   column(4,
                      # grid distance in km
                      numericInput("dist", "Grid distance:", 250),
 
@@ -71,30 +92,44 @@ navbarPage("Global Sampling Grid", id="nav",
                      numericInput("clusterpoints", "Points per cluster", 4),
 
                      # Cluster configuration
-                     selectInput("configuration", "Cluster configuration",
-                                 c("Line" = "line",
-                                   "L-shape" = "lshape",
-                                   "Square" = "square")),
+                     selectInput(
+                       "configuration",
+                       "Cluster configuration",
+                       c(
+                         "Line" = "line",
+                         "L-shape" = "lshape",
+                         "Square" = "square"
+                       )
+                     ),
 
                      # point distance
-                     sliderInput("pointdist", "Point distance:",
-                                 min=0, max=500, value=70),
+                     sliderInput(
+                       "pointdist",
+                       "Point distance:",
+                       min = 0,
+                       max = 500,
+                       value = 70
+                     ),
 
                      # Button "generate"
                      actionButton("reset_input", "Reset inputs"),
                      actionButton("go", "Generate")
-              ),
+                   ),
 
-              column(4,
+                   column(4,
                      h4("Download GSG"),
                      textOutput("text1"),
-                     selectInput("format", "Select output format:",
-                                 c("ESRI Shapefile" = "shp",
-                                   "KML" = "kml")),
-                     # Button "download"
+                     selectInput(
+                       "format",
+                       "Select output format:",
+                       c("ESRI Shapefile" = "shp",
+                         "KML" = "kml")
+                     ),
 
+                     # Button "download"
                      downloadButton("download", "Download")
-              )
-            ))
+
+                   )
+                 )))
   )
-))
+)
