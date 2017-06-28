a23898a27981d54ef5f6267370858a745f434805
Sebastian Schnell <Sebastian.Schnell@outlook.de>
2017-06-28 16:08:49 +0200

    Download fixed - gsg object was out of scope

--- Shiny/server.R
+++ Shiny/server.R
@@ -41,13 +41,13 @@ shinyServer(function(input, output, session) {
       setView(lng = 0, lat = 20, zoom = 3) %>%
 
       addWMSTiles("http://globalforestwatch-624153201.us-west-1.elb.amazonaws.com:80/arcgis/services/TreeCover2000/ImageServer/WMSServer",
-        layers = "0",
-        options = WMSTileOptions(format = "image/png", transparent = TRUE),
-        attribution = 'Hansen/UMD/Google/USGS/NASA, accessed through Global Forest Watch'
+                  layers = "0",
+                  options = WMSTileOptions(format = "image/png", transparent = TRUE),
+                  attribution = 'Hansen/UMD/Google/USGS/NASA, accessed through Global Forest Watch'
       ) #%>%
 
-      #addWMSLegend("http://globalforestwatch-624153201.us-west-1.elb.amazonaws.com:80/arcgis/services/TreeCover2000/ImageServer/WMSServer?request=GetLegendGraphic%26version=1.3.0%26format=image/png%26layer=0",
-      #position = 'bottomright')
+    #addWMSLegend("http://globalforestwatch-624153201.us-west-1.elb.amazonaws.com:80/arcgis/services/TreeCover2000/ImageServer/WMSServer?request=GetLegendGraphic%26version=1.3.0%26format=image/png%26layer=0",
+    #position = 'bottomright')
 
   })
 
@@ -169,43 +169,42 @@ shinyServer(function(input, output, session) {
                      message = 'Calculation in progress',
                      detail = 'Plotting the GSG...')
 
+        ### Export GSG ###
+
+        observeEvent(input$format,
+                     if (input$format == "shp") {
+                       output$download <- downloadHandler(
+                         filename = 'GSGExport.zip',
+                         content = function(file) {
+                           if (length(Sys.glob("GSGExport.*")) > 0) {
+                             file.remove(Sys.glob("GSGExport.*"))
+                           }
+                           writeOGR(gsg,
+                                    dsn = "GSGExport.shp",
+                                    layer = "GSGExport",
+                                    driver = "ESRI Shapefile")
+                           zip(zipfile = 'GSGExport.zip', files = Sys.glob("GSGExport.*"))
+                           file.copy("GSGExport.zip", file)
+                           if (length(Sys.glob("GSGExport.*")) > 0) {
+                             file.remove(Sys.glob("GSGExport.*"))
+                           }
+                         }
+                       )
+                     }
+                     else if (input$format == "kml") {
+                       output$download <- downloadHandler(
+                         filename = function() {
+                           paste0("GSG", input$dist, ".kml")
+                         },
+                         content = function(file) {
+                           writeOGR(gsg,
+                                    file,
+                                    layer = paste0("GSG", input$dist, "_KML"),
+                                    driver = "KML")
+                         }
+                       )
+                     })
+
       }
     })
-
-
-      ### Export GSG ###
-
-      observeEvent(input$format,
-                   if (input$format == "shp") {
-                     output$download <- downloadHandler(
-                       filename = 'GSGExport.zip',
-                       content = function(file) {
-                         if (length(Sys.glob("GSGExport.*")) > 0) {
-                           file.remove(Sys.glob("GSGExport.*"))
-                         }
-                         writeOGR(gsg,
-                                  dsn = "GSGExport.shp",
-                                  layer = "GSGExport",
-                                  driver = "ESRI Shapefile")
-                         zip(zipfile = 'GSGExport.zip', files = Sys.glob("GSGExport.*"))
-                         file.copy("GSGExport.zip", file)
-                         if (length(Sys.glob("GSGExport.*")) > 0) {
-                           file.remove(Sys.glob("GSGExport.*"))
-                         }
-                       }
-                     )
-                   }
-                   else if (input$format == "kml") {
-                     output$download <- downloadHandler(
-                       filename = function() {
-                         paste0("GSG", input$dist, ".kml")
-                       },
-                       content = function(file) {
-                         writeOGR(gsg,
-                                  file,
-                                  layer = paste0("GSG", input$dist, "_KML"),
-                                  driver = "KML")
-                       }
-                     )
-                   })
 })
