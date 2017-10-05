aecfc7d25b287cc493d9a464cc66c8861b7ae262
L. Fehrmann <L.Fehrmann@UFFE34.uni-forst.gwdg.de>
2017-10-05 11:35:12 +0200

    New version of Shiny app in dashboard design

---
+++ Shiny_new/server.R
@@ -0,0 +1,251 @@
+
+
+
+library(shiny)
+library(plotGoogleMaps)
+library(shinyjs)
+library(gsgsim)
+library(leaflet)
+library(leaflet.minicharts)
+library(raster)
+library(DT)
+
+#########################################################
+# global defenition of functions to store and retrieve data (see http://shiny.rstudio.com/articles/persistent-data-storage.html)
+# functions should later be moved to utilities or "global.R"
+
+saveData <- function(data) {
+  data <- as.data.frame(t(data))
+  if (exists("responses")) {
+    responses <<- rbind(responses, data)
+  } else {
+    responses <<- data
+  }
+}
+
+loadData <- function() {
+  if (exists("responses")) {
+    responses
+  }
+}
+##########################################################
+
+
+
+
+shinyServer(function(input, output, session) {
+
+
+  ## Small preview map ############
+  # Initial map (update after grid is generated, see leafletProxy)
+  output$preview <- renderLeaflet({
+    leaflet(height = "100%") %>%
+      addProviderTiles(providers$Stamen.TonerLite) %>%
+      setView(lng = 0,
+              lat = 20,
+              zoom = 2)
+  })
+
+
+
+  ## Grid generation #################
+
+  # Reset grid settings
+  observeEvent(input$reset_input, {
+    shinyjs::reset(id = "generate")
+  })
+
+  ### Enable generate button only when aoi is selected
+  observe({
+    toggleState(id = "go", condition = !is.null(input$country_code) | !is.null(input$aoi))
+  })
+
+
+
+
+  observeEvent(# Take a dependency on input$goButton. All calculations start after click only
+
+
+    input$go, {
+      # load selected aoi as boundary
+      in_bnd <- input$aoi
+
+      # getaoi depending on file format (currently only KML is used, but shp is possible. Radiobuttons for inputformat are commented in ui)
+      if (is.null(in_bnd)) {
+        getaoi <- NULL
+      } else {
+        switch(input$inputformat,
+               'kml' = {
+                 dir <- dirname(in_bnd[1, 4])
+                 file.rename(in_bnd[1, 4], paste0(dir, "/", in_bnd[1, 1]))
+
+                 getaoi <-
+                   list.files(dir, pattern = "*.kml", full.names = TRUE)
+               },
+
+               'shp' = {
+                 dir <- dirname(in_bnd[1, 4])
+
+                 for (i in 1:nrow(in_bnd)) {
+                   file.rename(in_bnd[i, 4], paste0(dir, "/", in_bnd[i, 1]))
+                 }
+
+                 getaoi <-
+                   list.files(dir, pattern = "*.shp", full.names = TRUE)
+
+               })
+      }
+
+      bnd <- isolate(load_boundary(
+        x = getaoi,
+        country_code = input$country_code,
+        adm_level = 0
+      ))
+
+      # generate GSG based on inputs from ui.R
+      # isolate () to avoid dependency on input$dist (but reactive on button)
+
+      gsg <- isolate(gen_gsg(input$dist, bnd))
+
+      # Show resulting sample size in valueBox
+      output$samplesize = renderInfoBox(infoBox(
+        length(gsg),
+        title = "sample size",
+        icon = icon("map-pin")
+      ))
+
+      # Update preview map
+
+      leafletProxy("preview", data = gsg) %>%
+        fitBounds(gsg@bbox[1, 1] - 1, gsg@bbox[2, 1] - 1, gsg@bbox[1, 2] +
+                    1, gsg@bbox[2, 2] + 1) %>%
+        clearShapes() %>%
+
+        addPolygons(
+          data = bnd,
+          #color = "#444444",
+          weight = 1,
+          smoothFactor = 0.5,
+          opacity = 0.3,
+          fillOpacity = 0.5
+        ) %>%
+
+
+        addCircles(
+          data = gsg,
+          weight = 3,
+          radius = 40,
+          color = "#CD0000",
+          stroke = TRUE,
+          fillOpacity = 0.9
+        )
+
+      # Update map for assessment
+      leafletProxy("googlemap", data = gsg) %>%
+        fitBounds(gsg@bbox[1,1]-1, gsg@bbox[2,1]-1, gsg@bbox[1,2]+1, gsg@bbox[2,2]+1) %>%
+        clearShapes() %>%
+
+        addAwesomeMarkers(data = gsg)
+
+
+      # addCircles(
+      #   data = gsg,
+      #   weight = 3,
+      #   radius = 40,
+      #   color = "#CD0000",
+      #   stroke = TRUE,
+      #   fillOpacity = 0.9
+      # )
+
+
+
+
+      ### Downloading the kml file #######
+
+      ### Enable download button when gsg is generated (inside observeEvent)
+      enable("download")
+
+      ### Export GSG to KML###
+      output$download <- downloadHandler(
+        filename = function() {
+          paste0("GSG", input$dist, ".kml")
+        },
+        content = function(file) {
+          writeOGR(gsg,
+                   file,
+                   layer = paste0("GSG", input$dist, "_KML"),
+                   driver = "KML")
+        }
+      )
+
+
+      # # Plot se_plot
+      # output$se_plot <- renderPlot({
+      #   se_binomial <- function(p){
+      #     se <- 100*sqrt((p*(1-p))/(length(gsg)-1))/p
+      #     return(se)
+      #   }
+      #
+      #   curve(se_binomial,
+      #         from=0.001,
+      #         to=1,
+      #         main = "Expected standard errors for grid points (no clusters)",
+      #         xlab="Expected area proportion of target class",
+      #         #log= "x",
+      #         ylab="Relative standard error (%)"
+      #   )
+      # })
+
+
+
+      # Generate Data Table for Data explorer
+
+      output$datatable <- DT::renderDataTable(DT::datatable(as.data.frame(gsg), options = list(pageLength = 25), selection = 'single'))
+
+      # Generate point list for navigation in "Assessment" (a short form of the data table)
+      output$pointlist <- DT::renderDataTable(DT::datatable(as.data.frame(gsg$`1:sum(idx)`, colnames("id")), options = list(pageLength = 5, searching = FALSE, filter = 'top'), selection = 'single'))
+
+      # Retrieve id (and lat long) of selected row in datatable (to zoom to this point)
+      id <- input$datatable_row_last_clicked
+
+      # Update maps
+
+
+
+
+
+    }) # ObserveEvent closed
+
+  ## Map for assessment ############
+  ## Initial map (update after grid is generated, see leafletProxy)
+  output$googlemap <- renderLeaflet({
+    leaflet() %>%
+      #addProviderTiles(providers$Esri.NatGeoWorldMap) %>%
+      addTiles(
+        urlTemplate = "https://mts1.google.com/vt/lyrs=s&hl=en&src=app&x={x}&y={y}&z={z}&s=G",
+        attribution = 'Google'
+      )
+
+
+
+
+
+
+
+
+  })
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
---
+++ Shiny_new/ui.R
@@ -0,0 +1,148 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "GSG"),
+
+  # Sidebar with menu
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Generate grid", tabName = "generate", icon = icon("gears")),
+      menuItem("Assessment", tabName = "assessment", icon = icon("eye")),
+      menuItem("Data Explorer", tabName = "explorer", icon = icon("table")),
+      menuItem("Analyze", tabName = "analyze", icon = icon("bar-chart"))
+    )
+  ),
+
+
+  dashboardBody(
+    tabItems(
+      tabItem("generate",
+              fluidRow(
+                useShinyjs(),
+                box(id = "generate",
+                    width = 6, solidHeader = TRUE,
+                    title = "GSG settings",
+
+                    # Select country code
+                    selectInput(
+                      "country_code",
+                      "Select countries",
+                      c("World", raster::ccodes()[, 1]),
+                      c("world", raster::ccodes()[, 2]),
+                      multiple = TRUE
+                    ),
+
+                    # File input for aoi
+                    fileInput("aoi",
+                              label = "Upload specific aoi as KML",
+                              accept = c('.shp', '.dbf', '.sbn', '.sbx', '.shx', ".prj", ".kml"),
+                              multiple = TRUE
+                    ),
+
+                    # grid distance in km
+                    numericInput("dist", "Grid distance in km", 250
+                    ),
+
+                    # Button "Reset"
+                    actionButton("reset_input", "Reset",
+                                 icon("refresh")
+                    ),
+                    # Button "Generate" (disabled until aoi is selected)
+                    shinyjs::disabled(actionButton("go", "Generate",
+                                                   icon("play")
+                    )),
+
+                    # Button "Download" (disabled until grid is generated)
+                    shinyjs::disabled(downloadButton("download", "Download"))
+
+                ),# box generate closed
+
+                # infoBox showing resulting sample size (only after grid is generated)
+                infoBoxOutput("samplesize")
+
+                # Preview map
+              ),
+              fluidRow(
+                box(
+                  width = 12, solidHeader = TRUE,
+                  title = "Preview",
+                  leafletOutput("preview",height = "50vh")
+                )
+              )
+      ), # tab generate closed
+
+      tabItem("assessment",
+              fluidRow(
+                box(id = "mapwindow", width = 8, height = "60vh",
+                    leafletOutput('googlemap', height = "58vh")
+
+
+                ),
+
+                box(id = "pointlist", width = 4, height = "60vh",
+                    title = "Point list",
+                    DT::dataTableOutput('pointlist')
+                )
+              ),
+
+              fluidRow(
+                tabBox(id = "variables", width = 12,
+                       title = "Assess variables",
+                       selected = "Tab3",
+                       tabPanel("Land Cover Components (LCC)", "LCC variables here"),
+                       tabPanel("Land use / function attributes (LUA)", "Tab content 2"),
+                       tabPanel("Landscape characteristics", "Note that when side=right, the tab order is reversed.")
+                )
+
+              )
+
+      ), # tab assessment closed
+
+      tabItem("explorer",
+              fluidRow(
+                box(width = 12,
+                    title = "Data Explorer",
+                    DT::dataTableOutput('datatable')
+                )
+              )
+
+
+      ), # tab explorer closed
+
+      tabItem("analyze",
+              fluidRow(
+                column(3,
+                       "Select variable"),
+
+                column(
+                  8,
+                  "Results",
+
+                  # tabsetPanel
+                  tabsetPanel(
+                    type = "tabs",
+
+                    tabPanel("Plot", plotOutput("plot")),
+
+                    tabPanel("Summary", verbatimTextOutput("summary")),
+
+                    tabPanel("Table", tableOutput("table"))
+                  )
+                )
+              )) # tab analyze closed
+    )
+
+
+
+
+
+  )
+)
